200c9dd17beaf56cc1d8cee5a6e47089c62220ed
ncale <nicholasbrodeur@outlook.com>
2024-03-16 19:41:19 +0000

    add contract address and abi

--- web/src/config.ts
+++ web/src/config.ts
@@ -1,5 +1,6 @@
-import { Address, parseAbi } from "viem";
+import { Address } from "viem";
+import { abi } from "./contractAbi";
 
-export const CONTRACT_ADDRESS = '' as Address
+export const CONTRACT_ADDRESS = '0x024908AE411d28DeDF86700C69b2F4A172FebCCc' as Address
 
-export const CONTRACT_ABI = parseAbi(['function transferFrom(address from, address to, uint256 tokenId)'])
+export const CONTRACT_ABI = abi
