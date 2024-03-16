6cfad9785f66673b23d00d1745661b52e5a91816
ncale <nicholasbrodeur@outlook.com>
2024-03-16 20:10:17 +0000

    add ponder endpoint

--- web/src/config.ts
+++ web/src/config.ts
@@ -1,6 +1,9 @@
 import { Address } from "viem";
 import { abi } from "./contractAbi";
 
+// define contract constants
 export const CONTRACT_ADDRESS = '0x024908AE411d28DeDF86700C69b2F4A172FebCCc' as Address
-
 export const CONTRACT_ABI = abi
+
+// define api constants
+export const PONDER_URL = 'https://hack-ponder-production.up.railway.app/'
