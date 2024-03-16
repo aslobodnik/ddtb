5381b859f01b74be0c137662f5a5c9ea95317d3b
ncale <nicholasbrodeur@outlook.com>
2024-03-16 18:16:45 +0000

    add contract transaction placeholder

--- web/src/frame/ddtb/transaction.ts
+++ web/src/frame/ddtb/transaction.ts
@@ -1,6 +1,13 @@
 import { TransactionContext } from 'frog'
-import { Address, parseEther } from 'viem'
+import { Address, parseAbi, parseEther } from 'viem'
+import { CONTRACT_ABI, CONTRACT_ADDRESS } from '../../config'
 
 export const transaction = async (c: TransactionContext) => {
-  return c.send({})
+  return c.contract({
+    to: CONTRACT_ADDRESS,
+    abi: CONTRACT_ABI,
+    chainId: 'eip155:8453',
+    functionName: 'transferFrom',
+    args: ['0x0000000000000000000000000000000000000000', '0x0000000000000000000000000000000000000000', 0n],
+  })
 }
