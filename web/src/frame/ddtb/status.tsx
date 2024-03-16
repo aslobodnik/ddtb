98edf8b6ae71152f8d70b10c658eb967c34c1ad8
ncale <nicholasbrodeur@outlook.com>
2024-03-16 16:46:07 +0000

    conditionally display status screen depending on if the user has the base

--- web/src/frame/ddtb/status.tsx
+++ web/src/frame/ddtb/status.tsx
@@ -1,15 +1,19 @@
 import { Button, FrameContext } from "frog"
+import { getEthAddressFromFid } from "../hub"
 import { viemClient } from "../viemClient"
-import { CONTRACT_ADDRESS } from "../../config"
 
 export const statusScreen = async (c: FrameContext) => {
 
-	const data = await viemClient.readContract({
-		address: CONTRACT_ADDRESS,
-		abi: (['abi']), // placeholder abi
-		functionName: '',
-	})
+	// get user fid from signed POST req
+  const { frameData } = c
+  const { fid } = frameData as { fid: number }
 	
+	// get verified addresses from fid
+	const userAddress = await getEthAddressFromFid(fid)
+	
+	// check if the user currently has the base
+	const hasBase = true // placeholder value   // viemClient.readContract({...})
+
 	const prevUser = 'limes.eth' // placeholder string
 	const currentUser = 'slobo.eth' // placeholder string
 	const timeRemaining = '00:02:56' // placeholder time
@@ -31,15 +35,16 @@ export const statusScreen = async (c: FrameContext) => {
 				}}>{prevUser} passed the base to {currentUser}</div>
 				<div style={{
 					display: 'flex'
-				}}>{currentUser} has {timeRemaining} to pass the BASE</div>
+				}}>{currentUser} has {timeRemaining} to pass the BASE {fid}</div>
 			</div>
     ),
     intents: [
       <Button action='/rules-screen-1'>Rules</Button>,
-			// if they have the BASE
-			<Button action='/pass'>Pass</Button>,
-			// if they don't have the BASE
-      <Button.Link href={`https://warpcast.com/${currentUser}`}>Remind {currentUser}</Button.Link>,
+			hasBase ? (
+				<Button action='/pass'>Pass</Button>
+			) : (
+				<Button.Link href={`https://warpcast.com/${currentUser}`}>Remind {currentUser}</Button.Link>
+			),
     ]
   })
 }
