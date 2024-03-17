f3b6a44cbc3bf7cf063ed1c06411a795ecb0dafc
ncale <nicholasbrodeur@outlook.com>
2024-03-17 02:50:01 +0000

    update contract address

--- web/src/config.ts
+++ web/src/config.ts
@@ -2,10 +2,10 @@ import { Address } from "viem";
 import { abi } from "./contractAbi";
 
 // define contract constants
-export const CONTRACT_ADDRESS = '0x024908AE411d28DeDF86700C69b2F4A172FebCCc' as Address
+export const CONTRACT_ADDRESS = '0xEC2244b547BD782FC7DeefC6d45E0B3a3cbD488d' as Address
 export const CONTRACT_ABI = abi
 
 // define api constants
 export const AIRSTACK_URL = 'https://api.airstack.xyz/graphql'
-export const AIRSTACK_API_KEY = process.env.AIRSTACK_API_KEY
+// export const AIRSTACK_API_KEY = process.env.AIRSTACK_API_KEY
 export const PONDER_URL = 'https://hack-ponder-production.up.railway.app/'
