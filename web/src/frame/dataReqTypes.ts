b50ac02ef9079e4f901cf770053f1b7772cf5167
ncale <nicholasbrodeur@outlook.com>
2024-03-17 00:14:32 +0000

    add types

--- web/src/frame/dataReqTypes.ts
+++ web/src/frame/dataReqTypes.ts
@@ -1,5 +1,29 @@
 import { Address } from "viem"
 
+export interface AirstackEthAddressesData {
+	data: Socials
+}
+interface Socials {
+	Socials: {
+		Social: UserAddresses[]
+	}
+}
+interface UserAddresses {
+	userAssociatedAddresses: Address
+}
+
+export interface AirstackEnsData {
+	data: {
+		Domains: {
+			Domain: Domain[]
+		}
+	}
+}
+interface Domain {
+	name: string
+	owner: Address
+}
+
 export interface PonderData {
 	baseToken: BaseToken
 }
