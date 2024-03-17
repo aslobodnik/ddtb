d623d9fce1bf2e87f1dabb7484aaa5e62bbe5017
ncale <nicholasbrodeur@outlook.com>
2024-03-17 00:14:15 +0000

    add airstack url and api key

--- web/src/config.ts
+++ web/src/config.ts
@@ -6,4 +6,6 @@ export const CONTRACT_ADDRESS = '0x024908AE411d28DeDF86700C69b2F4A172FebCCc' as 
 export const CONTRACT_ABI = abi
 
 // define api constants
+export const AIRSTACK_URL = 'https://api.airstack.xyz/graphql'
+export const AIRSTACK_API_KEY = process.env.AIRSTACK_API_KEY
 export const PONDER_URL = 'https://hack-ponder-production.up.railway.app/'
