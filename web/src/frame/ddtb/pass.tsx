622afe208083103c3a48977a55e24b7c648efd4d
enslimeseth <ens.limes.eth@gmail.com>
2024-03-17 03:10:05 +0000

    Fix Pass Page

--- web/src/frame/ddtb/pass.tsx
+++ web/src/frame/ddtb/pass.tsx
@@ -1,20 +1,12 @@
 import { FrameContext, TextInput } from "frog";
 import { Button } from "frog";
+import { ruleStyles } from "./styles";
 
 export const passScreen = (c: FrameContext) => {
   return c.res({
     image: (
       <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          color: "white",
-          fontSize: "50",
-          alignItems: "center",
-          backgroundColor: "white",
-          justifyContent: "center",
-        }}
-      >
+        style={{...ruleStyles}}>
         Enter the address of the user you want to pass the BASE to
       </div>
     ),
